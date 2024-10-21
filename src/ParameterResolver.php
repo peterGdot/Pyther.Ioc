843e55eb5b0327fcc5385231138303e6a979ddff
Peter Grundmann <mail@peter-grundmann.de>
2024-10-21 14:39:47 +0200

    fix to resolve nullable ctor/method arguments

--- src/ParameterResolver.php
+++ src/ParameterResolver.php
@@ -1,15 +1,14 @@
 <?php
 namespace Pyther\Ioc;
 
-
-abstract class ParameterResolver {
-
+abstract class ParameterResolver
+{
     /**
      * Resolve method or function parameters and handle dependencies.
      *
-     * @param Ioc $ioc The Ioc containter
-     * @param array $parameters List of methods parameters coming from reflection "getParameters".
-     * @param array $overrides Optional asocitative list of parameter values index by parameter name.
+     * @param Ioc $ioc The IoC container used to resolve dependencies.
+     * @param array $parameters List of ctor/method parameter coming from reflection "getParameters".
+     * @param array $overrides Optional associative list of parameters override values indexd by parameter name.
      * @return array Return the final array of parameter values.
      */
     public static function resolve(Ioc $ioc, array $parameters, array $overrides = []): array
@@ -23,9 +22,10 @@ abstract class ParameterResolver {
             if (isset($overrides[$name])) {
                 $resolved[] = $overrides[$name];
             } else {
+                $iocType = $type != null ? ltrim($type, "?") : null;
                 // parameter, that can be resolved?
-                if ($type != null && $ioc->hasBinding($type)) {
-                    $resolved[] = $ioc->resolve($type);
+                if ($iocType != null && $ioc->hasBinding($iocType)) {
+                    $resolved[] = $ioc->resolve($iocType);
                 }
                 // parameter has default value?
                 else if ($parameter->isDefaultValueAvailable()) {
