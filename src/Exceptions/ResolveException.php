2ef9105a456d0fb70801d8968cb448fc927236fa
Peter Grundmann <peter.grundmann.de@gmail.com>
2025-05-13 21:48:14 +0200

    remove php 8,4 warnings

--- src/Exceptions/ResolveException.php
+++ src/Exceptions/ResolveException.php
@@ -6,7 +6,7 @@ namespace Pyther\Ioc\Exceptions;
  */
 class ResolveException extends \Exception
 {
-    public function __construct(?string $message = null, int $code = 0, \Throwable $previous = null)
+    public function __construct(?string $message = null, int $code = 0, ?\Throwable $previous = null)
     {
         parent::__construct($message ?? "", $code, $previous);
     }
